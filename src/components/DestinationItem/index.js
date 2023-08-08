60cfde3bbdb9150d495a4f90d03925f7cab6ba1a
Akrishnaveni <125814322+Akrishnaveni@users.noreply.github.com>
2023-08-08 19:14:07 +0530

    Update index.js

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,14 +1,13 @@
-import "./index.css"
+import './index.css'
 
-class DestinationItem = props=>{
-const {destinationDetails}=props
-cont {id, name,imgUrl}=destinationDetails
-return (
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {name, imgUrl} = destinationDetails
+  return (
     <li className="destination-card-container">
-        <img src={imgUrl} alt={name} className="image"/>
-        <p className="description">{name}</p>
+      <img src={imgUrl} alt={name} className="image" />
+      <p className="description">{name}</p>
     </li>
-)
+  )
 }
 export default DestinationItem
-
